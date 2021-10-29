aec6996e825c87e7ac9cb04dae6617e41217394a
feng liang <feng.liang@kaust.edu.sa>
2021-10-30 00:13:55 +0300

    finished simple boundary collision

---
+++ code/billiard_game_single_ball.py
@@ -0,0 +1,50 @@
+import taichi as ti
+import numpy as np
+
+if __name__ == "__main__":
+    ti.init(ti.cpu)
+    resolution = (500, 500)
+    fps = 60
+    # world space [0.0, 1.0] ^ 2
+    velocity_magnitude_wc = 0.2
+    pixel_radius = 4
+    radius_wc = 1.0 / pixel_radius
+
+    velocity_wc = np.random.rand(2)
+    velocity_wc /= np.sqrt((velocity_wc ** 2).sum())
+    velocity_wc *= velocity_magnitude_wc / fps
+    pos_wc = np.random.rand(2)
+    gui = ti.GUI("billiard_game_single_ball", resolution)
+    gui.fps_limit = fps
+
+    boundary_begin = np.array([
+        [0.0, 0.0],
+        [0.0, 0.0],
+        [1.0, 1.0],
+        [1.0, 1.0]
+    ])
+
+    boundary_end = np.array([
+        [1.0, 0.0],
+        [0.0, 1.0],
+        [1.0, 0.0],
+        [0.0, 1.0]
+    ])
+
+    while gui.running:
+        gui.lines(begin=boundary_begin, end=boundary_end, radius=2)
+        gui.circle(pos_wc, radius=pixel_radius)
+        gui.show()
+        if 1.0 - radius_wc > pos_wc[0] > radius_wc:
+            pass
+        else:
+            velocity_wc[0] *= -1
+
+        if 1.0 - radius_wc > pos_wc[1] > radius_wc:
+            pass
+        else:
+            velocity_wc[1] *= -1
+
+        pos_wc += velocity_wc
+        pos_wc = np.minimum(1.0 - radius_wc, pos_wc)
+        pos_wc = np.maximum(radius_wc, pos_wc)
