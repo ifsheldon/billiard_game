46102d54e092d87a31740ba9e39c5709f743f0ac
feng liang <feng.liang@kaust.edu.sa>
2021-10-30 00:47:32 +0300

    finished translation with friction

--- code/billiard_game_single_ball.py
+++ code/billiard_game_single_ball.py
@@ -5,15 +5,17 @@ if __name__ == "__main__":
     ti.init(ti.cpu)
     resolution = (500, 500)
     fps = 60
+    g = 9.8
+    drag_coefficient = 0.01
     # world space [0.0, 1.0] ^ 2
-    velocity_magnitude_wc = 0.2
+    velocity_magnitude_wc = 1.0
     pixel_radius = 10
     radius_wc = 1.0 / resolution[0] * pixel_radius
 
     velocity_wc = np.random.rand(2)
     velocity_wc /= np.sqrt((velocity_wc ** 2).sum())
     velocity_wc *= velocity_magnitude_wc
-    pos_wc = np.random.rand(2)
+    pos_wc = np.ones(2) * 0.5
     gui = ti.GUI("billiard_game_single_ball", resolution)
     gui.fps_limit = fps
     delta_t = 1.0 / fps
@@ -46,6 +48,13 @@ if __name__ == "__main__":
         else:
             velocity_wc[1] *= -1
 
-        pos_wc += velocity_wc * delta_t
+        velocity_wc_dir = velocity_wc / np.sqrt((velocity_wc ** 2).sum())
+        drag_force = drag_coefficient * g * velocity_wc_dir
+
+        velocity_wc_next_t = velocity_wc - drag_force * delta_t
+        avg_velocity_vector = (velocity_wc + velocity_wc_next_t) / 2.
+        s = avg_velocity_vector * delta_t  # displacement vector
+        pos_wc += s
         pos_wc = np.minimum(1.0 - radius_wc, pos_wc)
         pos_wc = np.maximum(radius_wc, pos_wc)
+        velocity_wc = velocity_wc_next_t
