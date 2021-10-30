493c503aea69f77a775176d0bd5e451f69ff99aa
feng liang <feng.liang@kaust.edu.sa>
2021-10-30 21:15:47 +0300

    optimized algorithm a bit

--- code/billiard_game_single_ball.py
+++ code/billiard_game_single_ball.py
@@ -2,10 +2,13 @@ import taichi as ti
 import numpy as np
 
 
-def rectify_positions(bound_x_low, bound_x_high, bound_y_low, bound_y_high, positions):
+def rectify_positions_and_velocities(bound_x_low, bound_x_high, bound_y_low, bound_y_high, positions, velocities):
     # positions: (num, 2)
-    original_shape = positions.shape
+    # velocities: (num, 2)
+    original_p_shape = positions.shape
+    original_v_shape = velocities.shape
     positions = positions.reshape(-1, 2)
+    velocities = velocities.reshape(-1, 2)
     x_pos = positions[:, 0]
     y_pos = positions[:, 1]
     x_high_residual = np.maximum(0.0, x_pos - bound_x_high)
@@ -16,7 +19,11 @@ def rectify_positions(bound_x_low, bound_x_high, bound_y_low, bound_y_high, posi
     y_low_residual = np.minimum(0.0, y_pos - bound_y_low)
     positions[:, 0] -= x_low_residual
     positions[:, 1] -= y_low_residual
-    return positions.reshape(*original_shape)
+    need_reverse_x_velocity = (x_high_residual > 0.) | (x_low_residual < 0.)
+    need_reverse_y_velocity = (y_high_residual > 0.) | (y_low_residual < 0.)
+    velocities[need_reverse_x_velocity, 0] *= -1
+    velocities[need_reverse_y_velocity, 1] *= -1
+    return positions.reshape(*original_p_shape), velocities.reshape(*original_v_shape)
 
 
 if __name__ == "__main__":
@@ -59,16 +66,6 @@ if __name__ == "__main__":
         gui.lines(begin=boundary_begin, end=boundary_end, radius=2)
         gui.circle(pos_wc, radius=pixel_radius)
         gui.show()
-        if 1.0 - radius_wc > pos_wc[0] > radius_wc:
-            pass
-        else:
-            velocity_wc[0] *= -1
-
-        if 1.0 - radius_wc > pos_wc[1] > radius_wc:
-            pass
-        else:
-            velocity_wc[1] *= -1
-
         velocity_wc_dir = velocity_wc / np.sqrt((velocity_wc ** 2).sum())
         drag_force = drag_coefficient * g * velocity_wc_dir
 
@@ -76,7 +73,6 @@ if __name__ == "__main__":
         avg_velocity_vector = (velocity_wc + velocity_wc_next_t) / 2.
         s = avg_velocity_vector * delta_t  # displacement vector
         pos_wc += s
-        pos_wc = rectify_positions(virtual_bound_x[0], virtual_bound_x[1],
-                                   virtual_bound_y[0], virtual_bound_y[1],
-                                   pos_wc)
-        velocity_wc = velocity_wc_next_t
+        pos_wc, velocity_wc = rectify_positions_and_velocities(virtual_bound_x[0], virtual_bound_x[1],
+                                                               virtual_bound_y[0], virtual_bound_y[1],
+                                                               pos_wc, velocity_wc_next_t)
