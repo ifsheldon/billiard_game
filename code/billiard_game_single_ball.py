1955cfe29ed98b179595e5f975fb8f6a1dbd47a5
feng liang <feng.liang@kaust.edu.sa>
2021-10-30 20:58:56 +0300

    optimized algorithm a bit

--- code/billiard_game_single_ball.py
+++ code/billiard_game_single_ball.py
@@ -1,6 +1,24 @@
 import taichi as ti
 import numpy as np
 
+
+def rectify_positions(bound_x_low, bound_x_high, bound_y_low, bound_y_high, positions):
+    # positions: (num, 2)
+    original_shape = positions.shape
+    positions = positions.reshape(-1, 2)
+    x_pos = positions[:, 0]
+    y_pos = positions[:, 1]
+    x_high_residual = np.maximum(0.0, x_pos - bound_x_high)
+    y_high_residual = np.maximum(0.0, y_pos - bound_y_high)
+    positions[:, 0] -= x_high_residual
+    positions[:, 1] -= y_high_residual
+    x_low_residual = np.minimum(0.0, x_pos - bound_x_low)
+    y_low_residual = np.minimum(0.0, y_pos - bound_y_low)
+    positions[:, 0] -= x_low_residual
+    positions[:, 1] -= y_low_residual
+    return positions.reshape(*original_shape)
+
+
 if __name__ == "__main__":
     ti.init(ti.cpu)
     resolution = (500, 500)
@@ -34,6 +52,9 @@ if __name__ == "__main__":
         [0.0, 1.0]
     ])
 
+    virtual_bound_x = [radius_wc, 1.0 - radius_wc]
+    virtual_bound_y = [radius_wc, 1.0 - radius_wc]
+
     while gui.running:
         gui.lines(begin=boundary_begin, end=boundary_end, radius=2)
         gui.circle(pos_wc, radius=pixel_radius)
@@ -55,6 +76,7 @@ if __name__ == "__main__":
         avg_velocity_vector = (velocity_wc + velocity_wc_next_t) / 2.
         s = avg_velocity_vector * delta_t  # displacement vector
         pos_wc += s
-        pos_wc = np.minimum(1.0 - radius_wc, pos_wc)
-        pos_wc = np.maximum(radius_wc, pos_wc)
+        pos_wc = rectify_positions(virtual_bound_x[0], virtual_bound_x[1],
+                                   virtual_bound_y[0], virtual_bound_y[1],
+                                   pos_wc)
         velocity_wc = velocity_wc_next_t
