e02f40581ffcfa0e46b16e118562cf44667f1405
feng liang <feng.liang@kaust.edu.sa>
2021-11-17 22:16:44 +0300

    added place_balls_wc()

--- code/billiard_game_multi_ball.py
+++ code/billiard_game_multi_ball.py
@@ -6,6 +6,12 @@ from itertools import combinations
 from billiard_game_dual_ball import normalize_vector, two_ball_collides, calc_next_pos_and_velocity, \
     calc_after_collision_velocity, rectify_positions_in_collision, rectify_positions_and_velocities
 
+# Constants
+WHITE = 0xFFFFFF
+RED = 0xFF0000
+GREEN = 0x00FF00
+BLUE = 0x0000FF
+
 # wc for world space x[0.0, ratio],  y[0.0, 1.0]
 # sc for screen space [0.0, 1.0]^2
 
@@ -20,6 +26,7 @@ G = 9.8
 CUE_BALL_MAX_SPEED_WC = 1.0
 BALL_PIXEL_RADIUS = 10
 HOLE_PIXEL_RADIUS = 15
+num_balls = 11
 # Derived parameters
 ball_radius_wc = BALL_PIXEL_RADIUS / RESOLUTION[1]
 hole_radius_wc = HOLE_PIXEL_RADIUS / RESOLUTION[1]
@@ -36,8 +43,18 @@ def score(hole_center_positions, ball_position):
     return np.any(square_dist <= radii_square_sum)
 
 
+def place_balls_wc(span_wc, offset_wc):
+    ball_pos_wc = np.zeros((num_balls, 2))
+    for i in range(num_balls):
+        ball_i_pos_wc = np.random.rand(2) * span_wc + offset_wc
+        if i != CUE_BALL_IDX:
+            while two_ball_collides(ball_pos_wc[CUE_BALL_IDX], ball_i_pos_wc, ball_radius_wc):
+                ball_i_pos_wc = np.random.rand(2) * span_wc + offset_wc
+        ball_pos_wc[i] = ball_i_pos_wc
+    return ball_pos_wc
+
+
 if __name__ == "__main__":
-    num_balls = 11
     ti.init(ti.cpu)
 
     wc_to_sc_multiplier = np.array([1 / RATIO, 1])  # transform to [0,1]^ screen space
@@ -55,18 +72,12 @@ if __name__ == "__main__":
     hole_center_positions_wc = np.delete(hole_center_positions_wc, 4, axis=0)
     hole_center_positions_sc = hole_center_positions_wc * wc_to_sc_multiplier.reshape(1, 2)
 
-    ball_pos_wc = np.zeros((num_balls, 2))
     ball_velocities_wc = np.zeros((num_balls, 2))
     ball_visible = np.ones(num_balls, dtype=bool)
 
     span_wc = np.array([virtual_bound_x[1] - virtual_bound_x[0], virtual_bound_y[1] - virtual_bound_y[0]])
     offset_wc = np.array([virtual_bound_x[0], virtual_bound_y[0]])
-    for i in range(num_balls):
-        ball_i_pos_wc = np.random.rand(2) * span_wc + offset_wc
-        if i != CUE_BALL_IDX:
-            while two_ball_collides(ball_pos_wc[CUE_BALL_IDX], ball_i_pos_wc, ball_radius_wc):
-                ball_i_pos_wc = np.random.rand(2) * span_wc + offset_wc
-        ball_pos_wc[i] = ball_i_pos_wc
+    ball_pos_wc = place_balls_wc(span_wc, offset_wc)
 
     gui = ti.GUI("billiard_game_multi_ball", RESOLUTION)
     gui.fps_limit = FPS
@@ -91,9 +102,9 @@ if __name__ == "__main__":
     ball_pairs = list(combinations(range(num_balls), 2))
     ball_color_indices = np.ones(num_balls)
     ball_color_indices[CUE_BALL_IDX] = 0
-    ball_colors = [0xFFFFFF, 0xFF0000]
+    ball_colors = [WHITE, RED]
     while gui.running:
-        gui.clear(0x00FF00)
+        gui.clear(GREEN)
         hit_ball = gui.get_event(ti.GUI.PRESS) and gui.is_pressed("a")
         cue_ball_pos_sc = ball_pos_wc[CUE_BALL_IDX] * wc_to_sc_multiplier
         if np.allclose((ball_velocities_wc ** 2).sum(-1), 0., rtol=0.001, atol=0.001) and ball_visible[CUE_BALL_IDX]:
