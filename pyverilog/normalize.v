7ef81d7b96f9c90ec9924a81ca27c71e6d09d97f
Feng Liang <feng.liang@kaust.edu.sa>
2021-11-27 01:05:43 +0300

    add normalize, need debug

--- pyverilog/normalize.v
+++ pyverilog/normalize.v
@@ -2,26 +2,32 @@ module normalize
     #(parameter WIDTH = 32, FRAC_WIDTH = 30)
     (
         input clk,
+        input rst,
         input signed [WIDTH-1:0] x,
         input signed [WIDTH-1:0] y,
         input signed [WIDTH-1:0] z,
-        output reg done,
-        output reg [WIDTH-1:0] length,
-        output reg signed [WIDTH-1:0] nx,
-        output reg signed [WIDTH-1:0] ny,
-        output reg signed [WIDTH-1:0] nz
+        output done,
+        output signed [WIDTH-1:0] length,
+        output signed [WIDTH-1:0] nx,
+        output signed [WIDTH-1:0] ny,
+        output signed [WIDTH-1:0] nz,
+        output close_to_0
     );
-    reg [WIDTH*2 - 1:0] x_sqr;
-    reg [WIDTH*2 - 1:0] y_sqr;
-    reg [WIDTH*2 - 1:0] z_sqr;
-    reg [WIDTH - 1:0] sqr_sum;
-    reg close_to_0;
+    
+    wire [WIDTH - 1:0] length_sqr;
+
+    sqr_sum #(WIDTH, FRAC_WIDTH) square_sum(.x(x), .y(y), .z(z), .sum(length_sqr));
+
     sqrt #(WIDTH, FRAC_WIDTH) root(.clk(clk), 
-                                    .rst(?), 
-                                    .num_in(sqr_sum), 
+                                    .rst(rst), 
+                                    .num_in(length_sqr), 
                                     .done(done), 
                                     .out(length));
+
     close_to_zero #(WIDTH) judge_close_to_0 (length, close_to_0);
-    # TODO
+
+    assign nx = close_to_0? x : x / length;
+    assign ny = close_to_0? y : y / length;
+    assign nz = close_to_0? z : z / length;
 
 endmodule
