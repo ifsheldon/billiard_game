1788b96febdc6d28f55776433feba996b868993e
Feng Liang <feng.liang@kaust.edu.sa>
2021-11-27 22:03:52 +0300

    optimized code

--- pyverilog/close_to_zero.v
+++ pyverilog/close_to_zero.v
@@ -1,18 +1,12 @@
 module close_to_zero
     #(parameter WIDTH = 32, TOL = 10)
     (
-        input signed [WIDTH-1:0] num, // signed
-        output reg close
+        input signed [WIDTH-1:0] num,
+        output close
     );
     // TOL: tolerance - number of digits, if - 2^TOL <= num <= 2^TOL, return 1, else return 0
 
     reg signed [WIDTH-1:0] tolerance = 1'b1 << TOL;
-
-    always @* begin
-        if (num <= tolerance & num >= -tolerance)
-            close = 1;
-        else
-            close = 0;
-    end
+    assign close = num <= tolerance & num >= -tolerance;
 
 endmodule
