01b89447f2ee1050979f8d68cac609ff3099b390
Feng Liang <feng.liang@kaust.edu.sa>
2021-11-27 20:47:18 +0300

    fix normalize.v

--- pyverilog/normalize.v
+++ pyverilog/normalize.v
@@ -13,8 +13,10 @@ module normalize
         output signed [WIDTH-1:0] nz,
         output close_to_0
     );
-    
-    wire [WIDTH - 1:0] length_sqr;
+    parameter INT_WIDTH = WIDTH - FRAC_WIDTH;
+    parameter SHIFT_BIT = INT_WIDTH * INT_WIDTH - INT_WIDTH;
+
+    wire signed [WIDTH - 1:0] length_sqr;
 
     sqr_sum #(WIDTH, FRAC_WIDTH) square_sum(.x(x), .y(y), .z(z), .sum(length_sqr));
 
@@ -26,8 +28,16 @@ module normalize
 
     close_to_zero #(WIDTH) judge_close_to_0 (length, close_to_0);
 
-    assign nx = close_to_0? x : x / length;
-    assign ny = close_to_0? y : y / length;
-    assign nz = close_to_0? z : z / length;
+    wire signed [WIDTH * 2 - 1:0] shift_x = x << FRAC_WIDTH;
+    wire signed [WIDTH * 2 - 1:0] shift_y = y << FRAC_WIDTH;
+    wire signed [WIDTH * 2 - 1:0] shift_z = z << FRAC_WIDTH;
+    wire signed [WIDTH * 2 - 1:0] x_divide = (shift_x / length) << FRAC_WIDTH;
+    wire signed [WIDTH * 2 - 1:0] y_divide = (shift_y / length) << FRAC_WIDTH;
+    wire signed [WIDTH * 2 - 1:0] z_divide = (shift_z / length) << FRAC_WIDTH;
+
+ 
+    assign nx = close_to_0? x : (x_divide[2*WIDTH - 1:WIDTH] << SHIFT_BIT);
+    assign ny = close_to_0? y : (y_divide[2*WIDTH - 1:WIDTH] << SHIFT_BIT);
+    assign nz = close_to_0? z : (z_divide[2*WIDTH - 1:WIDTH] << SHIFT_BIT);
 
 endmodule
--- pyverilog/test_normalize.py
+++ pyverilog/test_normalize.py
@@ -8,26 +8,27 @@ def test_normalize():
     os.chdir("./pyverilog")
     sim = pyverilator.PyVerilator.build("normalize.v")
 
-    sim.clock.tick()
-    sim.io.rst = 1
-    sim.io.rst = 0
-
-    v = np.random.randn(3)
-    v /= np.sqrt((v ** 2).sum())
-    v *= np.sqrt(np.random.rand(1) * 2)  # magnitude will be [0, 2]
-    print(v)
-    v_fix_point_int = to_fix_point_int(v)
-    sim.io.x, sim.io.y, sim.io.z = v_fix_point_int
-    done = bool(sim.io.done.value)
-    while not done:
+    for i in range(10):
         sim.clock.tick()
-        done = bool(sim.io.done.value)
+        sim.io.rst = 1
+        sim.io.rst = 0
 
-    print(f"output length = {to_float(sim.io.length)}")
-    print(f"vector length = {np.sqrt((v ** 2).sum())}")
-    print(bool(sim.io.close_to_0.value))
-    # FIXME: now all the output is 0
-    normalize_v = to_float(np.array([sim.io.nx.value, sim.io.ny.value, sim.io.nz.value], dtype=np.int32))
-    expected_v = v / np.sqrt((v ** 2).sum())
-    print(f"result = {normalize_v}, length = {np.sqrt((normalize_v ** 2).sum())}")
-    print(f"expected = {expected_v}, length = {np.sqrt((expected_v ** 2).sum())}")
+        v = np.random.randn(3)
+        v /= np.sqrt((v ** 2).sum())
+        v *= np.sqrt(np.random.rand(1) * 2)  # magnitude will be [0, 2]
+        print(v)
+        v_fix_point_int = to_fix_point_int(v)
+        sim.io.x, sim.io.y, sim.io.z = v_fix_point_int
+        done = bool(sim.io.done.value)
+        while not done:
+            sim.clock.tick()
+            done = bool(sim.io.done.value)
+
+        print(f"output length = {to_float(sim.io.length)}")
+        print(f"vector length = {np.sqrt((v ** 2).sum())}")
+        print(f"vector close to zero: {bool(sim.io.close_to_0.value)}")
+        normalize_v = to_float(np.array([sim.io.nx.value, sim.io.ny.value, sim.io.nz.value], dtype=np.int32))
+        expected_v = v / np.sqrt((v ** 2).sum())
+        print(f"result = {normalize_v}, length = {np.sqrt((normalize_v ** 2).sum())}")
+        print(f"expected = {expected_v}, length = {np.sqrt((expected_v ** 2).sum())}")
+        assert np.allclose(normalize_v, expected_v, atol=0.0001)
